9e38ee57a8e92a017e7c142ecd90c6893758b8a0
trolland97 <trolland97@gmail.com>
2023-03-20 14:47:00 +0100

    init

---
+++ src/app/services/order-details.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderDetailsService {
+
+  constructor() { }
+
+  foodDetails = [
+    {
+      id:1,
+      foodName:"Grillezett szendvics",
+      foodDetails:"...",
+      foodPrice:200,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wzfq7djolqxgdhghebbq"
+    },
+    {
+      id:2,
+      foodName:"Vegetáriánus",
+      foodDetails:"...",
+      foodPrice:369,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/sgbobtbxlojbtdnr2m5k"
+    },
+    {
+      id:3,
+      foodName:"Burger",
+      foodDetails:"...",
+      foodPrice:149,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/xbeqlsck3p0kei53to7k"
+    },
+    {
+      id:4,
+      foodName:"Hús",
+      foodDetails:"...",
+      foodPrice:140,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/l2ng6gtge30sqaafqng7"
+    },
+    {
+      id:5,
+      foodName:"Hús",
+      foodDetails:"...",
+      foodPrice:105,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iqlmbg1hlyc0dspdyzzv"
+    },
+    {
+      id:6,
+      foodName:"Sajttorta",
+      foodDetails:"...",
+      foodPrice:219,
+      foodImg:"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/wtj8esaeslvlscv8glj6"
+    }
+  ]
+
+}
